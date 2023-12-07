74a788134530aa6f90a9a14177801bc4eacc2e33
sufian00t <130212322+sufian00t@users.noreply.github.com>
2023-12-07 22:50:11 +0600

    Create maximum-sum-with-exactly-k-elements.cpp

---
+++ maximum-sum-with-exactly-k-elements.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximizeSum(vector<int>& nums, int k) {
+        int maxi = *max_element(nums.begin() , nums.end()) ;
+        int ans = 0 ;
+        while(k--){
+            ans+=maxi ;
+            maxi++ ;
+        }
+        return ans ;
+    }
+};
