dbc008545bf209950985571a7473c7059743786e
Abu Sufian Rupok <abu115414@gmail.com>
2025-11-05 17:02:46 +0600

    Create Subsets.cpp

---
+++ Subsets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        int total = 1 << n;
+        vector<vector<int>> Subsets;
+    
+        for(int mask = 0; mask < total; mask++) {
+            vector<int> subset;
+           for(int i = 0; i < n; i++) {
+                if(mask & (1 << i)) subset.push_back(nums[i]);
+        }
+        Subsets.push_back(subset);
+    }
+      return Subsets;
+    }
+};
